9458970d3b7771af06fdd0107be2875678a9b173
ericxlima <eric.vinlima@gmail.com>
2022-12-08 13:30:01 -0300

    fix: resolve the quest making a recursive function

--- intervalos/main.py
+++ intervalos/main.py
@@ -11,24 +11,32 @@
 # Saída: [100-105], [110-111], [113-115], [150]
 
 
+def intervals(numbers_list:list[int], next_element=0) -> str:
+    """Recursive function that return a list of intervals 
+    from a list of sorted numbers."""
+    
+    if not numbers_list:
+        return "[]"
+    else:
+        if next_element == 0:
+            numbers_list[0] = f"[{numbers_list[0]}]"
+            return intervals(numbers_list, next_element=1)
+        else:
+            try:
+                current_element = numbers_list.pop(next_element)
+                element_ago_list = numbers_list[next_element-1][1:-1].split("-")
+                
+                first_element = int(element_ago_list[0])
+                last_element = int(element_ago_list[-1])
+                
+                if current_element == last_element + 1:
+                    new_element_ago = f"[{first_element}-{current_element}]"
+                    numbers_list[next_element-1] = new_element_ago
+                    return intervals(numbers_list, next_element=next_element)
+                else:
+                    numbers_list.insert(next_element, f"[{current_element}]")
+                    return intervals(numbers_list, next_element=next_element+1)
 
-def intervals(numbers_list):
-    #return [[numbers_list[0], numbers_list[-1]]]
-    interval = []
-    output = []
-    secundary = []
-
-    for position, i in enumerate(numbers_list):
-        if position == 0 or position == 1:
-            interval.append(i)
-        elif i == interval[-1] + 1:
-            interval[-1] = i
-        elif i != interval[-1] + 1:
-            secundary.append(i)
-
-    if interval:
-        output.append(interval)
-    if secundary:
-        output.append(secundary)
-
-    return output
+            except IndexError:
+                output = ", ".join(numbers_list)
+                return output.__str__().replace("'", "")
--- intervalos/test.py
+++ intervalos/test.py
@@ -2,19 +2,30 @@ import unittest
 from main import intervals
 
 class TestCase(unittest.TestCase):
+    
+    def test_no_elements(self):
+        result = intervals([])
+        self.assertEqual(result, "[]")
+    
+    def test_one_element(self):
+        result = intervals([3])
+        self.assertEqual(result, "[3]")
 
     def test_simple_list(self):
         result = intervals([100, 101])
-        self.assertEqual(result, [[100,101]])
+        self.assertEqual(result, "[100-101]")
 
     def test_three_numbers(self):
         result = intervals([1, 2, 3])
-        self.assertEqual(result, [[1, 3]])
+        self.assertEqual(result, "[1-3]")
 
-    def test_no_sequence(self):
+    def test_two_sequence(self):
         result = intervals([1,2,3,5,6])
-        self.assertEqual(result, [[1,3], [5,6]])
-
+        self.assertEqual(result, "[1-3], [5-6]")
+        
+    def test_many_sequences(self):
+        result = intervals([100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150])
+        self.assertEqual(result, "[100-105], [110-111], [113-115], [150]")
 
 
 if __name__ == "__main__":
